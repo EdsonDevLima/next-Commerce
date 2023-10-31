7987be45d195b24cd564f07cd268cf79d165305a
Edson <el002951@gmail.com>
2023-10-30 23:01:46 -0300

    listagem de produtos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,14 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-<div><p>next</p></div>
+<div className='max-w-7xl mx-auto pt-8 px-8 lx:px-0 '>
+  <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6'>
+    <div><p>prod 1</p></div>
+    <div><p>prod 1</p></div>
+    <div><p>prod 1</p></div>
+    <div><p>prod 1</p></div>
+
+  </div>
+</div>
   )
 }
